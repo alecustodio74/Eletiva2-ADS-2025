a6b3e2e82e25dd06a8a5e3ca58f17582e3089ba7
Alexandre Custódio <arcs1234@hotmail.com>
2025-08-26 21:16:40 -0300

    Primeiro commit

---
+++ primeiroprojeto/app/Http/Controllers/PrimeiraController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PrimeiraController extends Controller
+{
+    public function teste() {
+        return "Teste em laravel";
+    }
+
+    public function abrirForm() {
+        return view("exemplo"); 
+        
+    }
+
+    public function resposta(Request $request) {
+        $valor1 = $request->valor1;
+        $valor2 = $request->valor2;
+        $soma = $valor1 + $valor2;
+        return "Soma: ".$soma;
+    }
+}
